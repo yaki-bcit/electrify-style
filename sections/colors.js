0f6ca71eff75c4a4331fa1e6d8ab4dd55842d39c
Sohrab Radmehr <Sohrab.ram95@hotmail.com>
2022-10-23 12:40:59 -0700

    adding a picture

--- sections/colors.js
+++ sections/colors.js
@@ -1,8 +1,9 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import colorPhoto from "../public/Screenshot 2022-10-22 212409.png";
 
 // Sohrab
 export default function Colors() {
-
+  return <><Image src={colorPhoto} /></>;
 }
